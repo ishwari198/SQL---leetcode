414a8e83737e003b7a1c44b43605476dfad35d38
Ishwari Upadhyay <ishwariupadhyay85@gmail.com>
2024-07-03 09:32:33 -0400

    Time: 472 ms (90.10%) | Memory: 0B (100.00%) - LeetSync

--- 1942-primary-department-for-each-employee/primary-department-for-each-employee.sql
+++ 1942-primary-department-for-each-employee/primary-department-for-each-employee.sql
@@ -1,16 +1,11 @@
 # Write your MySQL query statement below
+select employee_id, department_id
+from employee 
+where primary_flag= 'Y'
+group by employee_id
 
-select 
-employee_id,
-department_id 
-from 
-Employee 
-where primary_flag = 'Y'
-UNION
-select 
-employee_id, department_id 
-from Employee 
-Group by 
-employee_id 
-Having 
-COUNT(employee_id) = 1
+union 
+select employee_id, department_id
+from employee
+group by employee_id
+having count(employee_id)=1
